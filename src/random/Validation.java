bf37331f21856753826a094ac3330224d9e7fc31
Student_2 <Student_2@165.24.36.141>
2018-03-12 15:14:39 -0700

    random

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -19,8 +19,27 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 3. Use each value of randomNumber to give the user a random compliment.
+if(randomNumber==0){
+	JOptionPane.showMessageDialog(null,"awesome");
+	}
+if(randomNumber==1){
+	JOptionPane.showMessageDialog(null,"amazing");
+	}		
+if(randomNumber==2){
+	JOptionPane.showMessageDialog(null,"great");
+	}
+if(randomNumber==3){
+	JOptionPane.showMessageDialog(null,"cool");
+	}
+if(randomNumber==4){
+	JOptionPane.showMessageDialog(null,"wow");
+	}
 
-		// 4. Repeat all the code above 10 times
+
+
+
+
+// 4. Repeat all the code above 10 times
 		
 		// 5. Find someone to test out your program. They will like it :)
 	}
